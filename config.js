896fdf6af7c83e99237f1e2c64dd12552241f3dc
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2020-10-19 15:23:15 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGODB:
-    "mongodb+srv://classesd:JHDBdndcN1jm7FHI@cluster0.yfbmb.mongodb.net/memg?retryWrites=true&w=majority",
-  SECRET_KEY: "some very secret key",
-  EXAMPLE: "",
-};
